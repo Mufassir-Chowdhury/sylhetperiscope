d26fd3ebf94cacc736a72d1d9b2ba3a48194da9b
Mufassir Ahmad Chowdhury <mac22214u@gmail.com>
2024-11-09 22:09:08 +0600

    added svg logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-    <img src="favicon.png" alt="Logo" class="object-cover w-40 h-40 mb-8 rounded-full">
+    <img src="logo.svg" alt="Logo" class="object-cover w-40 h-40 mb-8 rounded-full">
     <h1 class="text-4xl font-bold mb-4">আসছে শীঘ্রই</h1>
     <p class="text-lg mb-8 px-4 md:px-0">চমৎকার কিছু নিয়ে আসতে আমরা কাজ করে যাচ্ছি। অপেক্ষায় থাকুন!</p>
     <div class="flex justify-center items-center space-x-4">
